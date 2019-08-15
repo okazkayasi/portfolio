39208b316781e1e28acab735ba1c54a651b690ee
Oguz Kazkayasi <okazkayasi@outlook.com>
2019-08-15 10:49:56 +0300

    Added a post

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+# create a class in models.py
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')  # this file is in MEDIA_ROOT
+    summary = models.CharField(max_length=200)
